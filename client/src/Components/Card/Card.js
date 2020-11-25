09c60c79ad177030ff1d3f3cd0cc3ab525684f16
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-11-25 11:01:34 -0800

    modified card design and added like counter

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './_card.scss';
 
 function Card() {
+	const [likes, setLikes] = useState({ count: null });
+
+	const likeHandler = () => {
+		setLikes({ count: likes.count + 1 });
+	};
+
 	return (
 		<div className='card'>
-			<img src='https://picsum.photos/200/300' alt='' />
-			<div className='card_content'>
-				<h4>
-					UX-UI DESIGN <span class='project-name'>TEMPSO</span>
-				</h4>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
-					consequatur.
-				</p>
+			<div className='card-wrapper'>
+				<img src='https://picsum.photos/200/300' alt='' />
+				<div className='card-wrapper_actions'>
+					<h4>
+						UX-UI Design - <span class='project-name'>Tempso</span>
+					</h4>
+					<div className='icons'>
+						<div className='link'>
+							<a href='https://dribbble.com/Pariente'>
+								<i class='fas fa-link'></i>
+							</a>
+						</div>
+
+						<div className='likes' onClick={likeHandler}>
+							<i class='fas fa-heart'></i>
+							<p>{likes.count}</p>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className='underline'></div>
 		</div>
 	);
 }
